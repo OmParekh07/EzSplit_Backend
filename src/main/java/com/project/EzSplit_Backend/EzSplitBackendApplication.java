e22902adfffc3f839fb9ec3a27a772f06d62b303
OmParekh07 <parekhom001@gmail.com>
2026-03-13 14:15:38 +0530

    Intial setup

---
+++ src/main/java/com/project/EzSplit_Backend/EzSplitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.EzSplit_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzSplitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzSplitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/EzSplit_Backend/EzSplitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.EzSplit_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzSplitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
